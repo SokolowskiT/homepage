4adbb2027f64bdd2105a4265bcab5c1c8a1fe8c3
Tomasz Sokolowski <sokolowskit@gmail.com>
2019-12-20 14:27:21 +0100

    some JS

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 console.log("Witaj zbłąkany wędrowcze");
+
+const text = document.querySelector('.JSpractice__paragraph--JS');
+
+console.log(text.innerHTML);
+
+text.innerHTML = 'Ble ble ble - podmieniony tekst';
